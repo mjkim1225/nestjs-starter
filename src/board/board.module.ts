7879afa32caeee0c014d703f830017bb7f406628
김민정 <mjkim@gaia3d.com>
2023-02-18 23:46:05 +0900

    feat: typeorm entityrepository is deprecated, CustomRepository decorator 생성
    typeorm 0.3.x 버전부터는 entityRepository 가 없어졌다.
    따라서 데코레이터를 직접 생성하여 데코레이터가
    적용된 Repository를 받아줄 모듈을 사용하는 방법으로 해결했다.

--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BoardController } from './board.controller';
 import { BoardService } from './board.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardRepository } from './board.repository';
+import { TypeOrmExModule } from '../config/typerom-ex.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BoardRepository])],
+  imports: [TypeOrmExModule.forCustomRepository([BoardRepository])],
   controllers: [BoardController],
   providers: [BoardService],
 })
--- src/board/board.repository.ts
+++ src/board/board.repository.ts
@@ -1,4 +1,6 @@
 import { Repository } from 'typeorm';
 import { Board } from './board.entity';
+import { CustomRepository } from '../config/custom-repository.decorator';
 
+@CustomRepository(Board)
 export class BoardRepository extends Repository<Board> {}
