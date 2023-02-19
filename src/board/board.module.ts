2e5cd147c8f55ede41f645c463571fcaec544002
김민정 <mjkim@gaia3d.com>
2023-02-19 13:44:29 +0900

    feat: dir 구조 변경

--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { BoardController } from './controller/board.controller';
 import { BoardService } from './service/board.service';
 import { BoardRepository } from './repository/board.repository';
-import { TypeOrmExModule } from './repository/typerom-ex.module';
+import { TypeOrmExModule } from '../config/typerom-ex.module';
 
 @Module({
   imports: [TypeOrmExModule.forCustomRepository([BoardRepository])],
--- src/board/repository/board.repository.ts
+++ src/board/repository/board.repository.ts
@@ -1,6 +1,6 @@
 import { Repository } from 'typeorm';
 import { Board } from '../model/board.entity';
-import { CustomRepository } from './custom-repository.decorator';
+import { CustomRepository } from '../../config/custom-repository.decorator';
 
 @CustomRepository(Board)
 export class BoardRepository extends Repository<Board> {}
