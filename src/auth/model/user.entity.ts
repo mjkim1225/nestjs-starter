6748d8e849f1139860a2b5e32fb10ad8d59d9a34
김민정 <mjkim@gaia3d.com>
2023-02-19 14:44:56 +0900

    feat: username unique 설정, 같은 값 insert 시 예외 처리

--- src/auth/model/user.entity.ts
+++ src/auth/model/user.entity.ts
@@ -1,6 +1,13 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
 
 @Entity()
+@Unique(['username']) //username 이 unique해야함. 같은 값을 insert할 경우 예외를 던짐
 export class Users extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
--- src/auth/repository/user.repository.ts
+++ src/auth/repository/user.repository.ts
@@ -2,12 +2,24 @@ import { Users } from '../model/user.entity';
 import { CustomRepository } from '../../config/custom-repository.decorator';
 import { Repository } from 'typeorm';
 import { AuthCredentialDto } from '../model/auth-credential.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @CustomRepository(Users)
 export class UserRepository extends Repository<Users> {
   async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialDto;
     const user = this.create({ username, password });
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing user name');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
