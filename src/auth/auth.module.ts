1476968bb01422d8196a40cdc74c8abfc5e17237
김민정 <mjkim@gaia3d.com>
2023-02-19 19:00:07 +0900

    feat: jwt 로 토큰 생성하기
    jwt, passport 이용해서 로그인 성공시 json web token 생성 후 반환

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,20 @@ import { AuthController } from './controller/auth.controller';
 import { AuthService } from './service/auth.service';
 import { TypeOrmExModule } from '../config/typerom-ex.module';
 import { UserRepository } from './repository/user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmExModule.forCustomRepository([UserRepository])],
+  imports: [
+    JwtModule.register({
+      secret: 'secret-key', // secret key
+      signOptions: {
+        expiresIn: 60 * 60, // 유효 시간
+      },
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    TypeOrmExModule.forCustomRepository([UserRepository]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -3,23 +3,30 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from '../repository/user.repository';
 import { AuthCredentialDto } from '../model/auth-credential.dto';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
     return this.userRepository.createUser(authCredentialDto);
   }
 
-  async signIn(authCredentialDto: AuthCredentialDto): Promise<string> {
+  async signIn(
+    authCredentialDto: AuthCredentialDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialDto;
     const user = await this.userRepository.findOne({ where: { username } });
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'login success';
+      // 유저 토큰 생성 (Secret + Payload)
+      const payload = { username };
+      const accessToken = await this.jwtService.sign(payload);
+      return { accessToken };
     } else {
       throw new UnauthorizedException('login failed.');
     }
